476a60d1828ba1b5d82c78f0f9ded24cc1eea3b9
xKairu <kairu@team-reflex.com>
2023-05-02 19:42:41 +1200

    add missing pages

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,12 @@
+import { PartyPopper } from "lucide-react";
+
+export default function Page() {
+
+  return (
+    <div className="flex mt-6 items-center justify-center">
+      <PartyPopper />
+    </div>
+  );
+}
+
+export const runtime = "edge";
