bd176f02edd91ac506e49407cf6ae15a6631c418
xKairu <kairu@team-reflex.com>
2023-05-10 19:24:56 +1200

    seo stuff

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,51 +6,12 @@ import { cn } from "~/lib/utils"
 
 import { ClientProviders } from "../client-providers"
 
+export * from "~/config/metadata"
+
 const fontSans = Inter({
   weight: ["400", "500", "600", "800", "900"],
   subsets: ["latin"],
 })
-
-export const metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s · ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  keywords: ["kpop", "collection", "photocard", "album", "trading"],
-  authors: [
-    {
-      name: "Reflex",
-      url: siteConfig.links.github,
-    },
-  ],
-  creator: "Reflex",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  metadataBase: new URL(siteConfig.url),
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    creator: "@Reflexgg",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
-}
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
